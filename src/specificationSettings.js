0fdfafd850a1075fec5a6e52a15697df715e7424
huyiwei <448030519@qq.com>
2017-07-20 21:51:12 +0800

    change specificationSetting

--- src/specificationSettings.js
+++ src/specificationSettings.js
@@ -1,18 +1,22 @@
 /**
  * Created by MyLady on 16/2/19.
  */
+let discoveryHttpPort = 23001;
+let discoveryApiPrefix = '/api/v1';
+let proxyHttpPort = 23003;
 
 let specificationSettings = {
-    discoveryHttpPort: 23001,
-    discoveryApiPrefix: '/api/v1',
-    discoveryLocalHttpAddress: 'http://localhost:' + 23001 + '/api/v1',
-    discoveryApiService: 'http://localhost:' + 23001 + '/api/v1' + '/services',
-    discoveryApiProduct: 'http://localhost:' + 23001 + '/api/v1' + '/products',
+    discoveryHttpPort: discoveryHttpPort,
+    discoveryApiPrefix: discoveryApiPrefix,
+    discoveryLocalHttpAddress: 'http://localhost:' + discoveryHttpPort + discoveryApiPrefix,
+    discoveryApiService: 'http://localhost:' + discoveryHttpPort + discoveryApiPrefix+ '/services',
+    discoveryApiProduct: 'http://localhost:' + discoveryHttpPort + discoveryApiPrefix + '/products',
     registerInterval: 5000,
-    proxyHttpPort: 23003,
-    proxyHttpAddress: 'http://ip:' + 23003 + '/api/v1',
-    proxyApiService: 'http://ip:' + 23003 + '/api/v1' + '/services'
+    proxyHttpPort: discoveryHttpPort,
+    proxyHttpAddress: 'http://ip:' + proxyHttpPort + discoveryApiPrefix,
+    proxyApiService: 'http://ip:' + proxyHttpPort + discoveryApiPrefix + '/services'
 };
+
 
 Object.freeze(specificationSettings);
 export default specificationSettings;
