10ae4b1b1407166c4f530e940a83c152d3ff1c7c
mylady <closezling@outlook.com>
2017-12-14 15:30:41 +0800

    add new definition

---
+++ src/prisonTerminalType.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let prisonTerminalType = {
+    outsideTerminal: {
+      name: '监外屏',
+      value: 0
+    },
+    insideTerminal: {
+      name: '监内机',
+      value: 1
+    }
+  };
+  
+  Object.freeze(prisonTerminalType);
+  export default prisonTerminalType;
