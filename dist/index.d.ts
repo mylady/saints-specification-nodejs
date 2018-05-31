56ed91cd982cccbdf950480dc15df9c0af38339c
mylady <closezling@outlook.com>
2018-05-31 17:19:06 +0800

    fix typo

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -54,7 +54,7 @@ declare namespace spec {
         PlateNumber?: string;
         PlateType?: string;
         CarColor?: string;
-        PlateColor?: sring;
+        PlateColor?: string;
     }
 
     interface ConnectionModeStatic {
