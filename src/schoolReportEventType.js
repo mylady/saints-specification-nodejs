fc3c84e329565e54e771ff6fd73149b35f67dad3
mylady <closezling@outlook.com>
2019-03-04 18:55:05 +0800

    fix bug

--- src/schoolReportEventType.js
+++ src/schoolReportEventType.js
@@ -5,25 +5,25 @@ let schoolReportEventType = {
         name: '消防安全',
         value: 0
     },
-    unstableFactor:{
-        name:'不稳定因素',
-        value:1
+    unstableFactor: {
+        name: '不稳定因素',
+        value: 1
     },
     campusAround: {
         name: '校园周边',
         value: 2
     },
-    trafficSafety:{
-        name:'交通安全',
-        value:3
+    trafficSafety: {
+        name: '交通安全',
+        value: 3
     },
     foodSanitation: {
         name: '食品卫生及疫情',
         value: 4
     },
-    publicSentiment:{
-        name:'舆情',
-        value:5
+    publicSentiment: {
+        name: '舆情',
+        value: 5
     },
     accident: {
         name: '意外伤害',
@@ -33,9 +33,9 @@ let schoolReportEventType = {
         name: '刑事案件',
         value: 7
     },
-    others:{
-        name:'其他',
-        value:8
+    others: {
+        name: '其他',
+        value: 8
     }
 };
 
