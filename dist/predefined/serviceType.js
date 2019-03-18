923a1787bc21d09ec1a4873a45c7f85b8f5f572f
mylady <closezling@outlook.com>
2019-03-18 21:55:13 +0800

    add new service type

--- dist/predefined/serviceType.js
+++ dist/predefined/serviceType.js
@@ -8,7 +8,8 @@ var serviceType = {
   '0': '身份认证服务',
   '1': '设备信息服务',
   '2': '文件管理服务',
-  '3': '地理信息服务'
+  '3': '地理信息服务',
+  '4': '云代理服务'
 };
 Object.freeze(serviceType);
 var _default = serviceType;
