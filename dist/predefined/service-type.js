86d5d9d75b1fe4588427c81246749637d8a6a6d1
mylady <closezling@outlook.com>
2019-06-26 20:05:35 +0800

    update service-type

--- dist/predefined/service-type.js
+++ dist/predefined/service-type.js
@@ -6,13 +6,14 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var serviceType = {
   '0': '身份认证服务',
-  '1': '设备信息服务',
-  '2': '文件管理服务',
-  '3': '地理信息服务',
-  '4': '云代理服务',
-  '5': '日志服务',
+  '1': '文件管理服务',
+  '2': '设备信息服务',
+  '3': '云代理服务',
+  '4': '实时监控服务',
+  '5': '消息服务',
   '6': '事件服务',
-  '7': '时间流服务'
+  '7': '日志服务',
+  '8': '时间流服务'
 };
 Object.freeze(serviceType);
 var _default = serviceType;
