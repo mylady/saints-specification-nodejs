69af279d00807a84fa3b8641c9d5422a7643597e
mylady <closezling@outlook.com>
2019-12-30 21:05:30 +0800

    change folder name

--- src/klass/power-grid-value.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export default class PowerGridValue {
-    constructor({ volter, elec }) {
-        this.volter = volter || 0.0;
-        this.elec = elec || 0.0;
-    }
-}
--- src/predefined/internal-message-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const internalMessageType = {
-    '0':'设备状态',
-    '1':'设备事件'
-};
-
-Object.freeze(internalMessageType);
-export default internalMessageType;
