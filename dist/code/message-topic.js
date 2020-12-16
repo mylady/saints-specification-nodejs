3fccafdbab87ea70474e235b3a9b1fd88f22dada
mylady <closezling@outlook.com>
2020-12-16 19:11:47 +0800

    add value type to array

--- dist/code/message-topic.js
+++ dist/code/message-topic.js
@@ -4,13 +4,22 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var messageTopic = {
-  device_status: '设备状态',
-  device_event: '设备事件',
-  device_control: '设备控制',
-  custom: '自定义',
-  other: '其他'
-};
+var messageTopic = [{
+  name: 'saints-device-status',
+  value: '设备状态'
+}, {
+  name: 'saints-device-event',
+  value: '设备事件'
+}, {
+  name: 'saints-device-control',
+  value: '设备控制'
+}, {
+  name: 'saints-custom',
+  value: '自定义'
+}, {
+  name: 'saints-others',
+  value: '其他'
+}];
 Object.freeze(messageTopic);
 var _default = messageTopic;
 exports["default"] = _default;
--- src/code/message-topic.js
+++ src/code/message-topic.js
@@ -1,12 +1,27 @@
 'use strict';
 
-const messageTopic = {
-    device_status: '设备状态',
-    device_event: '设备事件',
-    device_control: '设备控制',
-    custom: '自定义',
-    other: '其他'
-};
+const messageTopic = [
+    {
+        name: 'saints-device-status',
+        value: '设备状态'
+    },
+    {
+        name: 'saints-device-event',
+        value: '设备事件'
+    },
+    {
+        name: 'saints-device-control',
+        value: '设备控制'
+    },
+    {
+        name: 'saints-custom',
+        value: '自定义'
+    },
+    {
+        name: 'saints-others',
+        value: '其他'
+    }
+];
 
 Object.freeze(messageTopic);
 export default messageTopic;
