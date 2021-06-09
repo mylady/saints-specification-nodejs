0f465868048b7e5508f01f2d51ef95871c2d9b55
mylady <closezling@outlook.com>
2021-06-10 00:28:24 +0800

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,11 @@
-import { CodeMap } from './code-map';
-import { Config } from './config';
-import { ResourceCode, DeviceAlarmCode, DeviceEventCode, DeviceStatusCode, DeviceFaultCode, InterfaceCategory } from './device-code';
-import { TwoDimension, ThreeDimension } from './dimension';
-import { SpecError } from './error';
-import { DoorValue, EnvMonitorValue, PowerGridValue, UPSValue, RFIDTagValue, CallValue, VideoAnalyseValue, VideoAnalyseType, Face, CarPlate, DeviceControlParam, InterfaceInitParam, InterfaceDescripter, DeviceControlCode, DeviceEventData, DeviceEventType } from './device-interface';
-import { Message, MessageType, MessageTopic } from './message';
-import { RestQuery, RestResponse } from './rest';
-import { Service, ServiceFinder, ServiceRegister, ServiceType } from './service';
-import { Option } from './option';
+export { CodeMap } from './code-map';
+export { Config } from './config';
+export { ResourceCode, DeviceAlarmCode, DeviceEventCode, DeviceStatusCode, DeviceFaultCode, InterfaceCategory } from './device-code';
+export { TwoDimension, ThreeDimension } from './dimension';
+export { SpecError } from './error';
+export { DoorValue, EnvMonitorValue, PowerGridValue, UPSValue, RFIDTagValue, CallValue, VideoAnalyseValue, VideoAnalyseType, Face, CarPlate, DeviceControlParam, InterfaceInitParam, InterfaceDescripter, DeviceControlCode, DeviceEventData, DeviceEventType } from './device-interface';
+export { Message, MessageType, MessageTopic } from './message';
+export { RestQuery, RestResponse } from './rest';
+export { Service, ServiceFinder, ServiceRegister, ServiceType } from './service';
 
-let InterfaceInitParamOption: Option.IInterfaceInitParamOption = {};
-let RestQueryOption: Option.IRestQueryOption = { start: 0 };
-let RestResponseOption: Option.IRestResponseOption = { result: true };
 
-export default {
-    CodeMap,
-    SpecError,
-    TwoDimension,
-    ThreeDimension,
-    Config,
-    ResourceCode,
-    InterfaceCategory,
-    InterfaceInitParamOption,
-    InterfaceInitParam,
-    Message,
-    MessageTopic,
-    MessageType,
-    DeviceAlarmCode,
-    DeviceEventCode,
-    DeviceStatusCode,
-    DeviceFaultCode,
-    DeviceControlCode,
-    DoorValue,
-    EnvMonitorValue,
-    PowerGridValue,
-    UPSValue,
-    RFIDTagValue,
-    CallValue,
-    VideoAnalyseValue,
-    VideoAnalyseType,
-    Face,
-    CarPlate,
-    RestQuery,
-    RestResponse,
-    RestQueryOption,
-    RestResponseOption,
-    DeviceControlParam,
-    InterfaceDescripter,
-    DeviceEventType,
-    DeviceEventData,
-    Service,
-    ServiceFinder,
-    ServiceRegister,
-    ServiceType
-}
